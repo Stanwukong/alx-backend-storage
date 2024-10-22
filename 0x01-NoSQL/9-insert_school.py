e6d489d8d98e7adb85e9b52482d5bc2c5b67754a
Stanwukong <stanleyajanaku@gmail.com>
2024-10-22 09:27:43 -0700

    Task 9: Adds a new document in a collection based on kwargs

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,16 @@
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document into a collection based
+    on keyword arguments (kwargs).
+
+    Args:
+        mongo_collection: The pymongo collection object.
+        **kwargs: The keyword arguments representing the
+                  fields and values for the new document.
+
+    Returns:
+        The _id of the newly inserted document.
+    """
+    result = mongo_collection.insert_one(kwargs)
+
+    return result.inserted_id
