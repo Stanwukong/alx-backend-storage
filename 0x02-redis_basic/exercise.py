9ae9cc04e049623bdb1611dce9244c89eeb0552a
Stanwukong <stanleyajanaku@gmail.com>
2024-10-26 00:19:54 +0000

    Tiny refactor

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""
-Writing strings to Redis with a cache class.
-"""
-import redis
+'''A module for using the Redis NoSQL data storage.
+'''
 import uuid
+import redis
 from functools import wraps
 from typing import Any, Callable, Union
 
@@ -65,39 +64,39 @@ def replay(fn: Callable) -> None:
 
 
 class Cache:
-    """A simple Cache class for interacting with Redis."""
-
-    def __init__(self):
-        """Initializes Redis client."""
+    '''Represents an object for storing data in a Redis data storage.
+    '''
+    def __init__(self) -> None:
+        '''Initializes a Cache instance.
+        '''
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data: Union[int, float, str, bytes]) -> str:
-        """
-        Store data in Redis with randomly generated key.
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
+    @call_history
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''Stores a value in a Redis data storage and returns the key.
+        '''
+        data_key = str(uuid.uuid4())
+        self._redis.set(data_key, data)
+        return data_key
 
-        return key
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        '''Retrieves a value from a Redis data storage.
+        '''
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
 
-     def get(self, 
-             key: str, 
-             fn: Callable = None
-             ) -> Union[str, bytes, int, float, None]:
-        """Retrieve data from Redis and apply a conversion
-            function.
-        """
-        value = self._redis.get(key)
-        if value is None:
-            return None
-
-        return fn(value) if fn else value
-
-    def get_str(self, key) -> str:
-        """Retrieve a string valie from Redis."""
-        return self.get(key, lambda d: d:decode("utf-8"))
+    def get_str(self, key: str) -> str:
+        '''Retrieves a string value from a Redis data storage.
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
-        """Retrieve an integer values from Redis."""
+        '''Retrieves an integer value from a Redis data storage.
+        '''
         return self.get(key, lambda x: int(x))
